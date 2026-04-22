41a1030a67187bf6e77e080f9aeb82863d60ff90
EfraimNascimento <60748643+EfraimNascimento@users.noreply.github.com>
2026-04-22 09:35:47 -0300

    Site Finalizado

--- js/main.js
+++ js/main.js
@@ -1,27 +1,66 @@
+let digital = document.querySelector("#digitalWatch");
+let id = 0;
 
-let digital = document.querySelector("#digitalWatch")
-let horario = 0
+document.getElementById('local').innerText = new Date().toLocaleDateString('pt-BR', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+});
 
 for(i = 0; i <= 2; i++){
     let p = document.createElement('p');
-    p.innerText = `${horario++}`
-    digital.appendChild(p)
-
-    
+    p.id = id++;
+    digital.appendChild(p);
 }
 
-let elements = digital.querySelectorAll('p');
-elements.forEach((element) =>{
-    element.innerText 
-})
+let time = {};
 
-// setInterval(() => {
-//     let date = new Date();
+setInterval(() =>{
+
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+    time = {
+        hour: hours,
+        minute: minutes,
+        second: seconds
+    };
+
+    let elements = digital.querySelectorAll('p');
+
+    elements.forEach((element) =>{
+
+        if(element.id == 0){
+            element.innerText = time.hour.toString().padStart(2, '0');
+        }
+        if(element.id == 1){
+            element.innerText = time.minute.toString().padStart(2, '0');
+        }
+        if(element.id == 2){
+            element.innerText = time.second.toString().padStart(2, '0');
+        }
+    })
+
+    let analogic = document.getElementById("#analogicWatch");
     
-//     let hours = date.getHours();
-//     let minutes = date.getMinutes();
-//     let seconds = date.getSeconds();
+    let analogicTime = {
+        hour: (hours * 30) + (minutes/2),
+        minute: (minutes * 6),
+        second: (seconds * 6)
+    };
+    
+    let pHora = document.getElementById("hora");
+    let pMinuto = document.getElementById("minuto");
+    let pSegundo = document.getElementById("segundo");
+    
+    pHora.style.transform = `translateX(-50%) rotate(${analogicTime.hour}deg)`;
+    
+    pMinuto.style.transform = `translateX(-50%) rotate(${analogicTime.minute}deg)`;
+    
+    pSegundo.style.transform = `translateX(-50%) rotate(${analogicTime.second}deg)`;
 
 
-//     console.log(date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds())
-// }, 1000)
+}, 100);
